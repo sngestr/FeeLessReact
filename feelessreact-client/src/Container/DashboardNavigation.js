732ea21b070730d7764f8e61ac6fffa0f38be9a9
Esther Song <esther.song62@myhunter.cuny.edu>
2017-12-09 21:21:12 -0500

    fix dashboard navigation

--- feelessreact-client/src/Container/DashboardNavigation.js
+++ feelessreact-client/src/Container/DashboardNavigation.js
@@ -1,15 +1,20 @@
 // Navigation bar for navigating through the Dashboard pages
 import React from 'react';
 import {Link} from 'react-router-dom';
+import './stylesheets/Dashboard.css';
 
 
 const DashboardNavigation = () => {
   return (
-    <div id="dash_nav">
-      REQUEST
-      HISTORY
-      PROFILE
-      REVIEW
+    <div id="dash_nav" className="container-fluid">
+    	<div className="row">
+    		 <span className="col-lg-2 text-center"></span>
+    		 <span className="col-lg-2 text-center dash_nav_link">Request</span>
+		     <span className="col-lg-2 text-center dash_nav_link">History</span>
+		     <span className="col-lg-2 text-center dash_nav_link">Profile</span>
+		     <span className="col-lg-2 text-center dash_nav_link">Review</span>
+		     <span className="col-lg-2 text-center"></span>
+    	</div>
     </div>
   );
 }
